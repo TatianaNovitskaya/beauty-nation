a6a667c109f55608cb80b0fdb9cbe36825fa216f
Tatiana Novitskaya <passatik11@gmail.com>
2023-10-16 13:31:20 -0400

    Added service section

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 
 import MainSection from "@/components/sections/MainSection";
 import Book from "@/components/sections/Book";
+import Service from "@/components/sections/Service";
 
 export default function Home() {
   return (
     <main className={"bg-orange-50"}>
       <MainSection/>
       <Book/>
+      <Service/>
     </main>
   )
 }
