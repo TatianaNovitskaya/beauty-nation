0e680375f5ad9972a6de92cfda169092bc975b1c
Tatiana Novitskaya <passatik11@gmail.com>
2023-10-16 19:04:32 -0400

    added text-shadow

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,16 +9,16 @@ export default function Header() {
         <header className={`fixed backdrop-blur-sm z-50 top-0 left-0 right-0 ${montserrat.className}`}>
             <div className={"container flex justify-between items-center py-5"}>
                 <div className={`logo ${monsieurLaDoulaise.className} text-5xl`}>
-                    <span>Beauty Nation Od</span>
+                    <span className={"drop-shadow-[2px_2px_2px_rgba(255,255,255,1)]"}>Beauty Nation Od</span>
                 </div>
                 <div className={"flex items-center text-white"}>
                     <nav className={"drop-shadow "}>
-                        <Link href="/#home" className={"mr-[50px]"}>Home</Link>
-                        <Link href="/#about" className={"mr-[50px]"}>About</Link>
-                        <Link href="/#products" className={"mr-[50px]"}>Products</Link>
-                        <Link href="/#contact" className={"mr-[50px]"}>Contact</Link>
+                        <Link href="/#home" className={"mr-[50px] drop-shadow-[1px_1px_1px_rgba(0,0,0,0.7)]"}>Home</Link>
+                        <Link href="/#about" className={"mr-[50px] drop-shadow-[1px_1px_1px_rgba(0,0,0,0.7)]"}>About</Link>
+                        <Link href="/#products" className={"mr-[50px] drop-shadow-[1px_1px_1px_rgba(0,0,0,0.7)]"}>Products</Link>
+                        <Link href="/#contact" className={"mr-[50px] drop-shadow-[1px_1px_1px_rgba(0,0,0,0.7)]"}>Contact</Link>
                     </nav>
-                    <button className={"border border-white rounded uppercase px-[2.3rem] py-4"}>Book</button>
+                    <button className={"border drop-shadow-[1px_1px_1px_rgba(0,0,0,0.7)] border-white rounded-[2rem] uppercase px-[2.3rem] py-4"}>Book</button>
                 </div>
             </div>
         </header>
