347748645767907c37edcd862e19de08334f0ef8
Tatiana Novitskaya <passatik11@gmail.com>
2023-10-16 22:48:32 -0400

    added mobile styles

--- src/components/sections/Book.js
+++ src/components/sections/Book.js
@@ -9,7 +9,7 @@ export default function Book() {
         <section className={"py-[2.75rem] md:py-[4.75rem]"}>
             <div className={"container flex h-full relative"}>
                 <form className={`${inter.className} w-full flex flex-col md:flex-row items-center justify-between`}>
-                    <div className={"flex flex-row items-center md:flex-col"}>
+                    <div className={"flex flex-row items-center mb-2.5 md:mb-0 md:flex-col"}>
                         <label className={"flex  items-center mr-[2rem] md:mr-0"}>
                             <Image src={"/calendar-frame.svg"} alt={"calendar"} width={14} height={14}
                                    className={"mr-[0.5rem]"}/>
@@ -18,21 +18,21 @@ export default function Book() {
                         <input type="date" required={true} id={"date"} min="2023-01-01" max="2023-12-31"
                                className={"bg-inherit w-full  mt-4 focus:outline-none placeholder:text-[#767676] border-b border-[#767676]"}/>
                     </div>
-                    <div className={"flex flex-row items-center md:flex-col"}>
+                    <div className={"flex flex-row items-center mb-2.5 md:mb-0 md:flex-col"}>
                         <label className={"flex items-center mr-[2rem] md:mr-0"}>
                             <Image src={"/person.svg"} alt={"person"} width={15} height={15} className={"mr-[0.5rem]"}/>
                             <span className={"uppercase text-[#D8B192] "}>name*</span>
                         </label>
                         <input type="text" required={true} placeholder={"Your name"} className={"bg-inherit w-full  mt-4 focus:outline-none placeholder:text-[#767676] border-b border-[#767676]"}/>
                     </div>
-                    <div className={"flex flex-row items-center md:flex-col"}>
+                    <div className={"flex flex-row items-center mb-2.5 md:mb-0 md:flex-col"}>
                         <label className={"flex items-center mr-[2rem] md:mr-0"}>
                             <Image src={"/phone.svg"} alt={"phone"} width={15} height={15} className={"mr-[0.5rem]"}/>
                             <span className={"uppercase text-[#D8B192] "}>phone*</span>
                         </label>
                         <input type="tel" required={true}  placeholder={"Your phone"} className={"bg-inherit w-full  mt-4 focus:outline-none placeholder:text-[#767676] border-b border-[#767676]"}/>
                     </div>
-                    <div className={"flex flex-row items-center md:flex-col"}>
+                    <div className={"flex flex-row items-center mb-2.5 md:mb-0 md:flex-col"}>
                         <label className={"flex items-center mr-[2rem] md:mr-0"}>
                             <Image src={"/cup.svg"} alt={"cup"} width={15} height={15} className={"mr-[0.5rem]"}/>
                             <span className={"uppercase text-[#D8B192] "}>service</span>
