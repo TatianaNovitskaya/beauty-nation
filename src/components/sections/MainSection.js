8f63d3e35e08f309e68922d75444c28e89e099ef
Tatiana Novitskaya <passatik11@gmail.com>
2023-10-16 22:31:19 -0400

    added mobile styles

--- src/components/sections/MainSection.js
+++ src/components/sections/MainSection.js
@@ -5,19 +5,19 @@ const parisienne = Parisienne({ subsets: ['latin'], weight: ["400"] })
 
 export default function MainSection(){
     return(
-        <section className={""}>
-            <div className={"container flex h-full relative"}>
-                <Image src={"/work-23.jpg"} alt={"work-23"} className={"absolute z-20 rounded-tl-[10rem] drop-shadow-2xl left-[25%] top-[50%] translate-x-1/2 rounded-tr-[10rem]"} width={310} height={838}/>
-                <div className={"w-1/2 pt-[6.25rem] pb-[8.25rem]"}>
-                    <div className={"relative mt-[5rem]"}>
-                        <h1 className={`${parisienne.className} font-bold text-[4rem] text-center `}>Nail Perfection, Your Reflection.</h1>
-                        <Image src={"/decor-3.png"} alt={"decor-3"} width={616} height={611} className={"absolute -top-[22rem] left-[11rem]"}/>
-                        <Image src={"/decor-1.png"} alt={"decor-2"} width={616} height={611} className={"absolute -bottom-[20rem] -left-[5rem]"}/>
-                        <Image src={"/nail-1.png"} alt={"nail"} width={441} height={294} className={"mx-auto z-10"}/>
+        <section>
+            <div className={"container flex flex-col md:flex-row h-full relative -mt-[5rem] md:m-0"}>
+                <Image src={"/work-23.jpg"} alt={"work-23"} className={"hidden md:block absolute z-20 rounded-tl-[10rem] drop-shadow-2xl left-[25%] top-[50%] translate-x-1/2 rounded-tr-[10rem]"} width={310} height={838}/>
+                <div className={"w-full py-[2.25rem]  md:w-1/2 md:pt-[6.25rem] md:pb-[8.25rem]"}>
+                    <div className={"relative mt-[3rem] md:mt-[5rem]"}>
+                        <h1 className={`${parisienne.className} font-bold text-[2.75rem] md:text-[4rem] text-center `}>Nail Perfection, Your Reflection.</h1>
+                        <Image src={"/decor-3.png"} alt={"decor-3"} width={616} height={611} className={" hidden md:block absolute -top-[22rem] left-[11rem]"}/>
+                        <Image src={"/decor-1.png"} alt={"decor-2"} width={616} height={611} className={"hidden md:block absolute -bottom-[20rem] -left-[5rem]"}/>
+                        <Image src={"/nail-1.png"} alt={"nail"} width={441} height={294} className={"hidden md:block mx-auto z-10"}/>
 
                     </div>
                 </div>
-                <div className={"w-1/2 relative"}>
+                <div className={"w-full md:w-1/2 h-[15rem] md:h-auto relative"}>
                     <Image src={"/work-11.jpg"} alt={"work-1"} className={"w-full"} style={{objectFit: 'cover'}} fill={true}/>
                 </div>
             </div>
